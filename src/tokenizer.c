f7db0a7ff363e2557a95710c8f11656e68ab0466
Daniel Moreno <dcmoreno17@gmail.com>
2021-03-01 14:05:05 -0700

    Added implementation for copy_str.  Modified word_terminator to return \0 at end of following the word pointer.  Modified space_char to also check for \n since fgets returns a new line at the end of inputted string

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 
-int space_char(char c)
+//added \n since fgets adds a new line to the end of string
+int space_char (char c)
 {
-  if(c==' ' || c == '\t') return 1;
+  if(c==' ' || c == '\t' || c=='\n') return 1;
   return 0;
 }
 
@@ -15,11 +17,9 @@ int non_space_char(char c)
 
 char *word_start(char *str)
 {
-  while(*str != '\0')
-  {
+  while (*str != '\0') {
     char curr_char = str[0];
-    if(space_char(curr_char) && non_space_char(str[1]))
-    {
+    if(space_char(curr_char) && non_space_char(str[1])) {
       *str++;
       break;
     }
@@ -30,17 +30,47 @@ char *word_start(char *str)
 
 char *word_terminator(char *word)
 {
-  while(*word != '\0') word++;
+  while(non_space_char(*word)) word++;
+  *word = '\0';
   return word;
 }
 
 int count_words(char *str)
 {
   int count = 0;
-  while(*str != '\0')
-  {
+  while(*str != '\0') {
     str = word_start(str);
     count++;
   }
   return count;
+}
+
+char *copy_str(char *inStr, short len)
+{
+  // NOTE: this implementation uses fgets, 1 is subtracted from the len
+  // in uimain.c since a \n char is added to the string
+  char *ptr=(char*)malloc((len+1)*sizeof(char));
+  if (ptr == NULL) printf("Memory not allocated.\n");
+
+  for (int i = 0; i < len; i++) {
+    *(ptr+i) = *(inStr+i);
+  }
+  *(ptr+len) = '\0';
+  return ptr;
+}
+
+void print_tokens(char **s)
+{
+  
+}
+
+void free_tokens(char **s)
+{
+  
+}
+
+char **tokenize(char *s)
+{
+  
+  return 0;
 }
--- src/uimain.c
+++ src/uimain.c
@@ -7,9 +7,13 @@ int main()
   
   puts("Enter a Word");
   printf("%s", "$ ");
-  char buf[MAX];
-  char *c = fgets(buf, MAX, stdin);
-  printf("%s", c);
+  char c[MAX];
+  fgets(c, MAX, stdin);
+
+  short len = word_terminator(c)-c;
+  char *strCopy = copy_str(c, len);
+  printf("Memory allocated: %i.  ", len);
+  printf("%s\n", strCopy);
   return 0;
 
 }
